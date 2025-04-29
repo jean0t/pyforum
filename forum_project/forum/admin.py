7f99af669af9ad56fddbc8d6f5f12a27951a1746
jean0t <joaomouryf@hotmail.com>
2025-04-28 21:22:43 -0300

    feat: admin panel configured

--- forum_project/forum/admin.py
+++ forum_project/forum/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Topic, Reply
 
 # Register your models here.
+class TopicAdmin(admin.ModelAdmin):
+    fields = ["title", "body", "author"]
+    list_display = ("title", "author", "created_at")
+
+class ReplyAdmin(admin.ModelAdmin):
+    fields = ["body", "author", "topic"]
+    list_display = ("author", "topic", "created_at")
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Reply, ReplyAdmin)
