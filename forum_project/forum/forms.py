00d3b11629ddede8fe92d05bda53b11f16b65e5d
jean0t <joaomouryf@hotmail.com>
2025-04-29 00:00:51 -0300

    feat: forms created for reply, topic and user signup; views also created to list the topics and to signup

---
+++ forum_project/forum/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import Topic, Reply
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class TopicForm(forms.ModelForm):
+    """
+    Form to create and edit Forum Topics
+    """
+    class Meta:
+        model = Topic
+        fields = ["title", "body"]
+
+        widgets = {
+            "title": forms.TextInput(attrs={"placeholder": "Topic Title"}),
+            "body": forms.Textarea(attrs={"rows": 5, "placeholder": "Write your topic here..."}),
+        }
+
+
+class ReplyForm(forms.ModelForm):
+    """
+    Form responsible to the replies to a Topic.
+    """
+    class Meta:
+        model = Reply
+        fields = ["body"]
+
+        widgets = {
+            "body": forms.Textarea(attrs={"rows": 3, "placeholder": "Write your reply..."}),
+        }
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
--- forum_project/forum/views.py
+++ forum_project/forum/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Topic
+from .forms import SignUpForm
+from django.contrib.auth import login
 
-# Create your views here.
+
+def topic_list(request):
+    topics = Topic.objects.all().order_by("-created_at")
+    return render(request, "forum/topic_list.html", {"topics": topics})
+
+
+def topic_detail(request, topic_id):
+    topic = get_object_or_404(Topic, id=topic_id)
+
+    replies = Reply.objects.filter(topic=topic).order_by("-created_at")
+
+    return render(request, "forum/topic_detail.html", {"topic": topic, "replies": replies})
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SingUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("topic_list")
+
+    else:
+        form = SignUpForm()
+
+    return render(request, "forum/signup.html", {"form": form})
